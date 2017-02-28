5b2ac62be8b3436ae425d02455409975ef67f94e
rodrigobell <rbell594@gmail.com>
2017-02-27 20:25:47 -0800

    Created view controller for user screen

---
+++ twitter-clone/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  twitter-clone
+//
+//  Created by Rodrigo Bell on 2/27/17.
+//  Copyright © 2017 Rodrigo Bell. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        self.navigationController?.navigationBar.barTintColor = UIColor.clear
+
+    }
+
+//    override func viewWillDisappear(_ animated: Bool) {
+//        self.navigationController?.navigationBar.barTintColor =  UIColor(red: 85.0/255.0, green: 172.0/255.0, blue: 253.0/255.0, alpha: 1.0)
+//    }
+
+}
